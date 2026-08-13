5471873b716af9ccb2e94d5cad6edb8ecb2ca0a8
Nishad Khandagale <nishadkhandagale82006@gmail.com>
2026-08-14 00:09:58 +0530

    Time: 13 ms (99.92%) | Memory: 42 MB (54.70%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.java
@@ -0,0 +1,21 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;// avoids over flow 
+
+            if (isBadVersion(mid)) {// mid is bad then we need to check on the left side L--------m------R  SEE NOW M=IS BAD WE CAN ALSO USE RIGHT = MID 
+                right = mid ;
+
+            } else { // MID IS ON RIGHT SIDE 
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
