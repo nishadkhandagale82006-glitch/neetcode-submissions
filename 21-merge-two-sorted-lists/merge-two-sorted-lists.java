19c5b7aaa04f3bb6fccf3b0c0fe4e6cb8e4fc229
Nishad Khandagale <nishadkhandagale82006@gmail.com>
2026-09-16 17:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (98.27%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,36 +1,31 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        // Dummy node to make merging easier
-        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        ListNode returnnode = new ListNode(Integer.MIN_VALUE); // Dummy node
+        ListNode headnode = returnnode; // Remember the start
 
-        // Pointer used to build the merged list
-        ListNode current = dummy;
+        while (list1 != null && list2 != null) { // Both lists must exist
 
-        // Compare nodes while both lists have nodes
-        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) { // Pick smaller
 
-            // Take the smaller node
-            if (list1.val <= list2.val) {
-                current.next = list1;
-                list1 = list1.next;
-            } else {
-                current.next = list2;
-                list2 = list2.next;
+                returnnode.next = list1; // Add list1 node
+                list1 = list1.next; // Move list1
+            }
+            else {
+
+                returnnode.next = list2; // Add list2 node
+                list2 = list2.next; // Move list2
             }
 
-            // Move current forward
-            current = current.next;
+            returnnode = returnnode.next; // Move result pointer
         }
 
-        // Attach whichever list is still remaining
-        if (list1 != null) {
-            current.next = list1;
-        } else {
-            current.next = list2;
-        }
+        // One list is empty → attach the other
+        if (list1 == null)
+            returnnode.next = list2;
+        else
+            returnnode.next = list1;
 
-        // Dummy node is not part of the answer
-        return dummy.next;
+        return headnode.next; // Return merged list
     }
 }
